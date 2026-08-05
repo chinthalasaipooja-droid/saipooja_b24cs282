c70265528f605f5484c2cef92445bb1b284543ed
chinthalasaipooja-droid <chinthalasaipooja@gmail.com>
2026-08-05 16:23:04 +0530

    Add files via upload

---
+++ event.js
@@ -0,0 +1,8 @@
+const EventEmitter=require("events");
+const e1 = new EventEmitter();
+e1.on("grade",(name,marks)=>
+{
+    console.log("Student Name:",name);
+    console.log("Marks:",marks);
+});
+e1.emit("grade","pooja",100);
